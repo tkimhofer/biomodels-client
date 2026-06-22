e76c7ec46970cbbf8efdab6bb675fc182b317e27
tk <tkimhofer@gmail.com>
2026-06-22 13:23:06 +0200

    upd dir

--- examples/wearable_to_trimp/01_import_apple_health.py
+++ examples/wearable_to_trimp/01_import_apple_health.py
@@ -2,7 +2,7 @@
 from biomodels_client.database import SensorStore
 from biomodels_client.importers.apple_health import AppleHealthImporter
 
-export_file = '/Users/tk/Downloads/export 3/apple_health_export/export.xml'#"PATH/TO/apple_health_export/export.xml"
+export_file = "PATH/TO/apple_health_export/export.xml"
 db_file = "wearables.sqlite"
 
 db = SensorStore(db_file)
